63f7a308a4e98aa9763bbfcc43f38a6974ac9fdf
sowmyakasiram <40853539+sowmyakasiram@users.noreply.github.com>
2025-11-06 15:17:44 +0530

    Implement merge function for sorted arrays

---
+++ Leetcode/88_merge_sorted_array.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 88_merge_sorted_array {
+    public static void main(String[] args) {
+        int nums1[]={1,2,3,0,0,0};
+        int nums2[]={2,5,6};
+        merge(nums1,3,nums2,3);
+        System.out.println("Num1 array:"+ Arrays.toString(nums1));
+
+    }
+
+        public static void merge(int[] nums1, int m, int[] nums2, int n) {
+            int i=m-1;
+            int j=n-1;
+            int k=m+n-1;
+
+            while(i>=0 && j>=0){
+                if(nums1[i]> nums2[j]){
+                    nums1[k]=nums1[i];
+                    i--;
+                }
+                else{
+                    nums1[k]=nums2[j];
+                    j--;
+                }
+                k--;
+            }
+
+            while(j>=0){
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
+        }
+
+
+}
